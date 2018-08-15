ac48c82119289cfe07fa85064b5b03e5fff1c900
levichen <levichen@nine-yi.com>
2018-08-15 13:24:40 +0800

    linked-list prepend, deleteHead, deleteTail

--- linked-list/LinkedList.js
+++ linked-list/LinkedList.js
@@ -28,6 +28,82 @@ class LinkedList {
   }
 
   /**
+   * @param {*} value
+   * @returns {LinkedList}
+   */
+  prepend (value) {
+    const newNode = new LinkedListNode(value, this.head)
+    this.head = newNode
+
+    if (!this.tail) {
+      this.tail = newNode
+    }
+
+    return this
+  }
+
+  /**
+   * @return {LinkedListNode}
+   */
+  deleteHead () {
+    if (!this.head) {
+      return null
+    }
+
+    const deletedHead = this.head
+
+    if (this.head.next) {
+      this.head = this.head.next
+    } else {
+      this.head = null
+      this.tail = null
+    }
+
+    return deletedHead
+  }
+
+  /**
+   * @return {LinkedListNode}
+   */
+  deleteTail () {
+    const deletedTail = this.tail
+    let currentNode = this.head
+
+    if (this.head === this.tail) {
+      this.head = null
+      this.tail = null
+
+      return deletedTail
+    }
+
+    while (currentNode.next !== null) {
+      if (currentNode.next.next === null) {
+        currentNode.next = null
+      } else {
+        currentNode = currentNode.next
+      }
+    }
+
+    this.tail = currentNode
+
+    return deletedTail
+  }
+
+   /**
+   * @param {Object} findParams
+   * @param {*} findParams.value
+   * @param {function} [findParams.callback]
+   * @return {LinkedListNode}
+   */
+  find ({ value = undefined, callback = undefined }) {}
+
+   /**
+   * @param {*} value
+   * @return {LinkedListNode}
+   */
+  delete (value) {}
+
+  /**
    * @returns {LinkedListNode[]}
    */
   toArray () {
