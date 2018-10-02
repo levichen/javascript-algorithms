aa96ed877bda223ed5cc8ea092c410413fe11d85
levichen <levichen@nine-yi.com>
2018-10-02 13:01:16 +0800

    init avl tree

---
+++ data-structures/9-tree/AvlTree.js
@@ -0,0 +1,57 @@
+const BinarySearchTree = require('../binary-search-tree/BinarySearchTree')
+
+export default class AvlTree extends BinarySearchTree {
+  /**
+   * @param {*} value
+   */
+  insert (value) {
+    // Do the normal BST insert.
+    super.insert(value)
+
+    // Let's move up to the root and check balance factors along the way.
+    let currentNode = this.root.find(value)
+    while (currentNode) {
+      this.balance(currentNode)
+      currentNode = currentNode.parent
+    }
+  }
+
+  /**
+   * @param {*} value
+   * @return {boolean}
+   */
+  remove (value) {
+
+  }
+
+  /**
+   * @param {BinarySearchTreeNode} node
+   */
+  balance (node) {
+
+  }
+
+  /**
+   * @param {BinarySearchTreeNode} rootNode
+   */
+  rotateLeftLeft (rootNode) {
+  }
+
+  /**
+   * @param {BinarySearchTreeNode} rootNode
+   */
+  rotateLeftRight (rootNode) {
+  }
+
+  /**
+   * @param {BinarySearchTreeNode} rootNode
+   */
+  rotateRightLeft (rootNode) {
+  }
+
+  /**
+   * @param {BinarySearchTreeNode} rootNode
+   */
+  rotateRightRight (rootNode) {
+  }
+}
